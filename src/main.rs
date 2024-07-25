f7dcd07a7d5b757488bb2b48a98582eb3b272f96
Jonas Dietrich <diejoc20@htl-kaindorf.at>
2024-07-25 09:10:50 +0200

    na hawi des suit jetzt oba gonz fix gengan :pray:

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
-use axum::{http::{header::ACCESS_CONTROL_ALLOW_ORIGIN, HeaderValue}, response::{IntoResponse, Response}, routing::{get, post}, Json, Router};
+use axum::{response::{IntoResponse, Response}, routing::{get, post}, Json, Router};
 
-use serde_json::{Value, json};
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
@@ -9,7 +9,10 @@ async fn main() {
 
     let app = Router::new()
         .route("/start", post(startserver))
-        .route("/power", get(is_server_powered_on));
+        .route("/power", get(is_server_powered_on))
+        .route("/startmc", post(start_mc))
+        .route("/stopmc", post(stop_mc))
+        .route("/mcstatus", get(get_kurbling));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
@@ -52,3 +55,53 @@ async fn startserver() -> Response {
         Json(json!({"message" : gyatt}))
     ).into_response()
 } 
+
+async fn start_mc() -> Response {
+    let commandpath = "/home/hawara/starter.sh";
+
+    let res = Command::new(commandpath)
+        .output()
+        .expect("Failed to execute!");
+
+    let gyatt = String::from_utf8(res.stdout).unwrap();
+    println!("started: {gyatt}");
+
+    (
+        [("Access-Control-Allow-Origin", "*")],
+        Json(json!({"message" : "oida"}))
+    ).into_response()
+}
+
+async fn stop_mc() -> Response {
+    let commandpath = "/home/hawara/ender.sh";
+
+    let res = Command::new(commandpath)
+        .output()
+        .expect("Failed to execute!");
+
+    let gyatt = String::from_utf8(res.stdout).unwrap();
+    println!("stopped: {gyatt}");
+
+    (
+        [("Access-Control-Allow-Origin", "*")],
+        Json(json!({"message" : "na hawi"}))
+    ).into_response()
+}
+
+async fn get_kurbling() -> Response {
+    let commandpath = "/home/hawara/checker.sh";
+
+    let res = Command::new(commandpath)
+        .output()
+        .expect("Failed to execute!");
+
+    let gyatt = String::from_utf8(res.stdout).unwrap();
+    let status = gyatt.contains("lock");
+
+    println!("status: {status}");
+
+    (
+        [("Access-Control-Allow-Origin", "*")],
+        Json(json!({"on" : status}))
+    ).into_response()
+}
